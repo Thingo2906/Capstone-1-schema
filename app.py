7bdb535663dceb95d4db756881db0ec5902c18a5
Suong <thanhsuong1805@gmail.com>
2023-01-04 16:28:45 -0800

    new commit

--- app.py
+++ app.py
@@ -22,6 +22,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
+print('********************')
+print('********************')
+print(app.config['SECRET_KEY'])
 
 toolbar = DebugToolbarExtension(app)
 
